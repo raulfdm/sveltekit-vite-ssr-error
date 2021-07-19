ac8307f294bd9771dcdc20e78891848ce04220a7
Raul Melo <melo.raulf@gmail.com>
2021-07-19 19:07:03 +0200

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,7 @@
+<script>
+	import { unified } from 'unified';
+	console.log(unified);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
